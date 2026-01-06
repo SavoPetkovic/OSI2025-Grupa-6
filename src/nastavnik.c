e6dc5db5478ba78e3245299c76211c9bcda94774
marijarulic <marija.rulic@student.etf.unibl.org>
2026-01-06 10:58:27 +0100

    #12 implementirano zakljucivanje ocjena

--- src/nastavnik.c
+++ src/nastavnik.c
@@ -49,6 +49,7 @@ void nastavnikMeni()
         printf("\n=== NASTAVNIK MENI ===\n");
         printf("1. Evidencija izostanka ucenika\n");
         printf("2. Unos opravdanja izostanka\n");
+        printf("3. Zakljucivanje ocjene\n");
         printf("0. Izlaz\n");
         printf("Izbor: ");
         scanf("%d", &izbor);
@@ -59,6 +60,9 @@ void nastavnikMeni()
                 break;
             case 2:
                 unesiOpravdanje();
+                break;
+            case 3:
+                zakljuciOcjenu();
                 break;
             case 0:
                 printf("Izlaz iz nastavnickog menija.\n");
@@ -133,4 +137,118 @@ void unesiOpravdanje()
         printf("Opravdanje je uspjesno uneseno.\n");
     else
         printf("Ne postoji neopravdan izostanak za unijete podatke.\n");
+}
+
+void zakljuciOcjenu()
+{
+    FILE *fOcjene, *fZakljucne, *fPeriod;
+    char ime[50], prezime[50], predmet[50];
+    char linija[300];
+    int ocjena, suma = 0, broj = 0;
+    float prosjek;
+    int zakljucna;
+    char potvrda;
+
+    printf("\n=== ZAKLJUCIVANJE OCJENE ===\n");
+
+    /* Provjera perioda */
+    fPeriod = fopen("period.txt", "r");
+    if (!fPeriod) {
+        printf("Period zakljucivanja nije definisan.\n");
+        return;
+    }
+
+    fgets(linija, sizeof(linija), fPeriod);
+    fclose(fPeriod);
+
+    if (strstr(linija, "AKTIVAN") == NULL) {
+        printf("Zakljucivanje ocjena trenutno NIJE dozvoljeno.\n");
+        return;
+    }
+
+    printf("Ime ucenika: ");
+    scanf("%49s", ime);
+
+    printf("Prezime ucenika: ");
+    scanf("%49s", prezime);
+
+    printf("Predmet: ");
+    scanf(" %[^\n]%*c", predmet);
+
+    /* Provjera da li je vec zakljucena ocjena */
+    fZakljucne = fopen("zakljucne_ocjene.txt", "r");
+    if (fZakljucne) {
+        while (fgets(linija, sizeof(linija), fZakljucne)) {
+            if (strstr(linija, ime) &&
+                strstr(linija, prezime) &&
+                strstr(linija, predmet)) {
+
+                printf("Ocjena je vec zakljucena i ne moze se mijenjati.\n");
+                fclose(fZakljucne);
+                return;
+            }
+        }
+        fclose(fZakljucne);
+    }
+
+    /* Citanje ocjena */
+    fOcjene = fopen("ocjene.txt", "r");
+    if (!fOcjene) {
+        printf("Nema unesenih ocjena.\n");
+        return;
+    }
+
+    printf("\nOcjene:\n");
+    while (fgets(linija, sizeof(linija), fOcjene)) {
+        if (strstr(linija, ime) &&
+            strstr(linija, prezime) &&
+            strstr(linija, predmet)) {
+
+            sscanf(linija, "%*[^0-9]%d", &ocjena);
+            printf("- %d\n", ocjena);
+            suma += ocjena;
+            broj++;
+        }
+    }
+    fclose(fOcjene);
+
+    if (broj == 0) {
+        printf("Nema dovoljno ocjena za zakljucivanje.\n");
+        return;
+    }
+
+    prosjek = (float)suma / broj;
+    zakljucna = (int)(prosjek + 0.5); // zaokruzivanje
+
+    printf("\nPredlozena zakljucna ocjena: %d\n", zakljucna);
+    printf("Zelite li izmijeniti ocjenu? (Y/N): ");
+    scanf(" %c", &potvrda);
+
+    if (potvrda == 'Y' || potvrda == 'y') {
+        printf("Unesite zakljucnu ocjenu (1-5): ");
+        scanf("%d", &zakljucna);
+    }
+
+    printf("Potvrditi zakljucivanje? (Y/N): ");
+    scanf(" %c", &potvrda);
+
+    if (potvrda != 'Y' && potvrda != 'y') {
+        printf("Zakljucivanje otkazano.\n");
+        return;
+    }
+
+    /* Upis zakljucne ocjene */
+    fZakljucne = fopen("zakljucne_ocjene.txt", "a");
+    if (!fZakljucne) {
+        printf("Greska pri upisu zakljucne ocjene.\n");
+        return;
+    }
+
+    fprintf(fZakljucne,
+        "Ucenik: %s %s | Predmet: %s | Zakljucna: %d\n",
+        ime, prezime, predmet, zakljucna);
+
+    fclose(fZakljucne);
+
+    printf("Zakljucna ocjena je USPJESNO sacuvana.\n");
 }
